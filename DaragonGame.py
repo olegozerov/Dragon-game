1ad35d0b36912327e7521a82d981e80f372152b5
olegozerov <ozerovoleg92@gmail.com>
2021-09-05 22:21:11 +0600

    Создал общий клас для всех гироев, создал клас для дракона и клас для рыцаря

---
+++ DaragonGame.py
@@ -0,0 +1,35 @@
+class Hiroes:
+    """
+    hp - жизненная энергия, запас здоровья
+    defence - защита
+    srong - сила
+    weapon - оружие
+    shield - щит
+    magic - магия
+    """
+    def __init__(self, hp, defence, strong, weapon, shield, magic):
+        self.hp = hp
+        self.defence = defence
+        self.strong = strong
+        self.weapon = weapon
+        self.shield = shield
+        self.magic = magic
+
+
+class Dragon(Hiroes):
+    def __init__(self, hp=2000, defence=120, strong=150, weapon=0):
+        self.hp = hp
+        self.defence = defence
+        self.strong = strong
+        self.weapon = weapon
+
+
+class Knights(Hiroes):
+    def __init__(self, hp=1000, defence=100, strong=120, weapon=250, shield=150):
+        self.hp = hp
+        self.defence = defence
+        self.strong = strong
+        self.weapon = weapon
+        self.shield = shield
+
+        
