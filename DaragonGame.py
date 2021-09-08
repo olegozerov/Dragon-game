f359c0d9056697ca1232897529c299b2ab17268c
olegozerov <ozerovoleg92@gmail.com>
2021-09-09 00:28:22 +0600

    Добавлены: методы урон для рыцаря в класс DragonGame и удар в родительский класс Hero

--- DaragonGame.py
+++ DaragonGame.py
@@ -1,37 +1,86 @@
+import random
+
+
 class Hiroes:
 
     """Тут мы плодим наших героев"""
 
-    def __init__(self, hp, defence, strong, weapon, shield, magic):
-        self.hp = hp                #  hp - жизненная энергия, запас здоровья
-        self.defence = defence      #  defence - защита
-        self.strong = strong        #  srong - сила
-        self.weapon = weapon        #  weapon - оружие
-        self.shield = shield        #  shield - щит
-        self.magic = magic          #  magic - магия
+    def __init__(self, hp=0, defence=0, strong=0, weapon=0, shield=0):
+        self._hp = hp                #  hp - жизненная энергия, запас здоровья
+        self._defence = defence      #  defence - защита
+        self._strong = strong        #  srong - сила
+        self._weapon = weapon        #  weapon - оружие
+        self._shield = shield        #  shield - щит
+
+
+    def modify_health(self, parametr):
+        if self._hp > 0:
+            if parametr > 0:
+                self._hp += parametr
+                return self._hp
+            else:
+                self._hp += parametr
+                return self._hp
+
+    def hit(self):
+        return self._strong + self._weapon
 
 
 class Dragon(Hiroes):
+
+    """Дракон обладает следующими характреистиками: hp, defence, strong, weapon"""
+
     def __init__(self, hp=2000, defence=120, strong=150, weapon=0):
-        self.hp = hp
-        self.defence = defence
-        self.strong = strong
-        self.weapon = weapon
+        self._hp = hp                #  hp - жизненная энергия, запас здоровья
+        self._defence = defence      #  defence - защита
+        self._strong = strong        #  srong - сила
+        self._weapon = weapon        #  weapon - оружие
 
+    def __str__(self):
+        return f"Характеристики дрокона: hp = {self._hp}\n \
+                       defence = {self._defence}\n \
+                       strong = {self._strong}\n \
+                       weapon = {self._weapon}\n"
 
+                
 class Knights(Hiroes):
+
+    """Рыцарь обладает следующими характреистиками: hp, defence, strong, weapon, shield"""
+
     def __init__(self, hp=1000, defence=100, strong=120, weapon=250, shield=150):
-        self.hp = hp
-        self.defence = defence
-        self.strong = strong
-        self.weapon = weapon
-        self.shield = shield
-
-
-
+        self._hp = hp                #  hp - жизненная энергия, запас здоровья
+        self._defence = defence      #  defence - защита
+        self._strong = strong        #  srong - сила
+        self._weapon = weapon        #  weapon - оружие
+        self._shield = shield        #  shield - щит
+    
+    def __str__(self):
+        return f"Характеристики дрокона: hp = {self._hp}\n \
+                       defence = {self._defence}\n \
+                       strong = {self._strong}\n \
+                       weapon = {self._weapon}\n \
+                       shield = {self._shield}\n"
+  
+        
 class DragonGame:
 
     """Здесь происходит сражение"""
-    
-    while True:
+
+    dragon = Dragon()
+    knights = Knights()
+
+    def knight_damage(self):
+        if random.random() < 0.75:
+            self.damage = self.knights.hit() - self.dragon._defence
+            self.dragon._hp -= self.damage
+            print(f"\nДракон получил {self.damage} едениц урона \n") 
+   
+        
+
+    def dragon_damage(self):
         pass
+
+
+d = DragonGame()
+
+d.knight_damage()
