8ad5b5efc1f51437271371181f9a35fa660301cf
olegozerov <ozerovoleg92@gmail.com>
2021-09-11 19:49:57 +0600

    Добавил услове при котором герои не могут наносить уров в соответствующие методы #3

--- DaragonGame.py
+++ DaragonGame.py
@@ -19,18 +19,25 @@ class Hiro:
         else:
             return self._hp
 
+    def hiroe_lose(self):
+        if self._hp <= 0:
+            print(f"\n{self.__repr__()} умер \n")
+
     def hit(self):
         return self._strong + self._weapon
+
 
 class Dragon(Hiro):
 
     """Дракон обладает следующими характреистиками: hp, defence, strong, weapon"""
 
-    def __init__(self, hp=2000, defence=120, strong=150, weapon=0):
+    def __init__(self, hp=400, defence=120, strong=150, weapon=0):
         self._hp = hp                #  hp - жизненная энергия, запас здоровья
         self._defence = defence      #  defence - защита
         self._strong = strong        #  srong - сила
         self._weapon = weapon        #  weapon - оружие
+
+    
 
     def __str__(self):
         return f"Характеристики дрокона: hp = {self._hp}\n \
@@ -38,12 +45,14 @@ class Dragon(Hiro):
                        strong = {self._strong}\n \
                        weapon = {self._weapon}\n"
 
+    def __repr__(self):
+        return f"{'Дракон'}"
                 
 class Knight(Hiro):
 
     """Рыцарь обладает следующими характреистиками: hp, defence, strong, weapon, shield"""
 
-    def __init__(self, hp=1000, defence=100, strong=120, weapon=250, shield=150):
+    def __init__(self, hp=70, defence=100, strong=120, weapon=250, shield=150):
         self._hp = hp                #  hp - жизненная энергия, запас здоровья
         self._defence = defence      #  defence - защита
         self._strong = strong        #  srong - сила
@@ -57,6 +66,8 @@ class Knight(Hiro):
                        weapon = {self._weapon}\n \
                        shield = {self._shield}\n"
 
+    def __repr__(self):
+        return f"{'Рыцарь'}"
 
 class Damage():
 
@@ -66,24 +77,29 @@ class Damage():
     knight = Knight()
 
     def knight_damage(self):
-        if random.random() < 0.75:
-            self.damage = self.knight.hit() - self.dragon._defence
-            self.dragon._hp -= self.damage
-            print(f"\nРыцарь нанес удар и дракон получил {self.damage} единиц урона")
-            print(f"И теперь у дракона осталось {self.dragon.health_tracking()} единиц здоровья \n")
-        else:
-            print(f"\nРыцарь не смог попасть и нанести урон дракону")
-            print(f"У дракона все еще {self.dragon.health_tracking()} единиц здоровья \n")
+        if self.knight._hp > 0:
+            if random.random() < 0.75:
+                self.damage = self.knight.hit() - self.dragon._defence
+                self.dragon._hp -= self.damage
+                print(f"\nРыцарь нанес удар и дракон получил {self.damage} единиц урона")
+                print(f"И теперь у дракона осталось {self.dragon.health_tracking()} единиц здоровья \n")
+                self.dragon.hiroe_lose()
+            else:
+                print(f"\nРыцарь не смог попасть и нанести урон дракону")
+                print(f"У дракона все еще {self.dragon.health_tracking()} единиц здоровья \n")
+
 
     def dragon_damage(self):
-        if random.random() < 0.50:
-            self.damage = self.dragon.hit() - self.knight._defence
-            self.knight._hp -= self.damage
-            print(f"\nДракон нанес удар и рыцарь получил {self.damage} единиц урона") 
-            print(f"И теперь у рыцаря осталось {self.knight.health_tracking()} единиц здоровья \n")
-        else:
-            print(f"\nДракон проспал свой ход")
-            print(f"У рыцаря все еще {self.knight.health_tracking()} единиц здоровья \n")
+        if self.dragon._hp > 0:
+            if random.random() < 0.50:
+                self.damage = self.dragon.hit() - self.knight._defence
+                self.knight._hp -= self.damage
+                print(f"\nДракон нанес удар и рыцарь получил {self.damage} единиц урона") 
+                print(f"И теперь у рыцаря осталось {self.knight.health_tracking()} единиц здоровья \n")
+                self.knight.hiroe_lose()
+            else:
+                print(f"\nДракон проспал свой ход")
+                print(f"У рыцаря все еще {self.knight.health_tracking()} единиц здоровья \n")
 
 
 class DragonGame:
@@ -91,16 +107,32 @@ class DragonGame:
     """Здесь происходит сражение"""
 
     damage = Damage()
-
+  
     while True:
-        break    
-
+        if damage.dragon._hp > 0:
+            damage.knight_damage()
+        else:
+            break
+        if damage.knight._hp > 0:
+            damage.dragon_damage()
+        else:
+            break
 
 
 
 d = Damage()
 
-d.dragon_damage()
+print(d.dragon)
+print(d.knight)
+
+"""d.dragon_damage()
 d.knight
 d.dragon_damage()
 d.knight
+print(d.knight)"""
+
+"""d.knight_damage()
+d.dragon
+d.knight_damage()
+d.dragon
+print(d.dragon)"""
