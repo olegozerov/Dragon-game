2e58f713ca8d54feb6bac1103c7d7718655da35b
olegozerov <ozerovoleg92@gmail.com>
2021-09-18 21:45:57 +0600

    Добавлена графика и переработан вывод информации с действиями героев

--- DaragonGame.py
+++ DaragonGame.py
@@ -1,6 +1,7 @@
 import random
+from imageACS import *
  
-class Hiro:
+class Hero:
  
     """Тут мы плодим наших героев"""
  
@@ -21,12 +22,15 @@ class Hiro:
  
     def hiroe_lose(self):
         if self._hp <= 0:
-            print(f"\n{self.__repr__()} умер \n")
+            print("+" + "-" * 150 + "+")
+            print(f"{self.__repr__()} умер ")
+            print("+" + "-" * 150 + "+")
+            print(win)
  
     def hit(self):
         return self._strong + self._weapon
  
-class Dragon(Hiro):
+class Dragon(Hero):
  
     """Дракон обладает следующими характеристиками: hp, defence, strong, weapon"""
  
@@ -48,7 +52,7 @@ class Dragon(Hiro):
     def __repr__(self):
         return f"{'Дракон'}"
                 
-class Knight(Hiro):
+class Knight(Hero):
  
     """Рыцарь обладает следующими характеристиками: hp, defence, strong, weapon, shield"""
  
@@ -67,7 +71,10 @@ class Knight(Hiro):
         if Knight.HAS_SHIELD == False:
             self._defence += self._shield
             Knight.HAS_SHIELD = True
-            print(f"\nРыцарь надел свой щит, его защита теперь {self._defence} единиц \n")
+            print(knight_shield)
+            print("+" + "-" * 150 + "+")
+            print(f"Рыцарь надел свой щит, его защита теперь {self._defence} единиц")
+            print("+" + "-" * 150 + "+")
             return Knight.HAS_SHIELD
         else:
             pass
@@ -77,7 +84,6 @@ class Knight(Hiro):
             if Knight.HAS_SHIELD == True:
                 self._defence -= self._shield
                 Knight.HAS_SHIELD = False
-                print(f"\nРыцарь снял свой щит, его защита теперь {self._defence} единиц \n")
                 return Knight.HAS_SHIELD
             else:
                 pass
@@ -85,12 +91,18 @@ class Knight(Hiro):
     def get_potion(self):
         if self._potion_butle > 0:
             self._potion_butle -= 1
-            print(f'Осталось бутылочек с зельем {self._potion_butle}')
+            print(bottle)
+            print("+" + "-" * 150 + "+")
+            print(f"Рыцарь выпил зелье и его здоровье увеличилось на {self._potion_portion} единиц")
+            print(f"Осталось бутылочек с зельем {self._potion_butle}")
+            print("+" + "-" * 150 + "+")
             self._hp += self._potion_portion
             return self._hp
         else:
+            print("+" + "-" * 150 + "+")
             print("\nБутылочки с зельем закончились \n")
             print("Выбирете другое действие рыцаря \n")
+            print("+" + "-" * 150 + "+")
 
   
     def __str__(self):
@@ -118,20 +130,34 @@ class Damage():
     def dragon_damage_whith_fire_ball(self):
         if self.knight.HAS_SHIELD == False:
             self.knight._hp -= self.dragon.fire_ball()
-            print(f"\nДракон нанес удар огненным шаром и рыцарь получил {self.dragon.fire_ball()} единиц урона") 
-            print(f"И теперь у рыцаря осталось {self.knight.health_tracking()} единиц здоровья \n")
+            print(dragon_att_fire)
+            print("+" + "-" * 150 + "+")
+            print(f"Дракон нанес удар огненем и рыцарь получил {self.dragon.fire_ball()} единиц урона") 
+            print(f"И теперь у рыцаря осталось {self.knight.health_tracking()} единиц здоровья")
+            print("+" + "-" * 150 + "+")
             self.knight.hiroe_lose()
         else:
-            print(f"\nДракон изверг огонь, но не смог нанести урон. Рыцарь был под щитом")
+            print(fack)
+            print("+" + "-" * 150 + "+")
+            print(f"Дракон изверг огонь, но не смог нанести урон. Рыцарь был под щитом")
+            print("+" + "-" * 150 + "+")
+
  
     def dragon_damage_whithout_fire_ball(self):
         if self.dragon.hit() > self.knight._defence:
             self.knight._hp -= self.dragon.hit() - self.knight._defence
-            print(f"\nДракон нанес удар и рыцарь получил {self.dragon.hit() - self.knight._defence} единиц урона") 
-            print(f"И теперь у рыцаря осталось {self.knight.health_tracking()} единиц здоровья \n")
+            print(dragon_att)
+            print("+" + "-" * 150 + "+")
+            print(f"Дракон нанес удар и рыцарь получил {self.dragon.hit() - self.knight._defence} единиц урона") 
+            print(f"И теперь у рыцаря осталось {self.knight.health_tracking()} единиц здоровья ")
+            print("+" + "-" * 150 + "+")
             self.knight.hiroe_lose()
         else:
-            print(f"\nДракон нанес удар рыцарю, но не смог нанести урон. Рыцарь был под щитом")
+            print(fack)
+            print("+" + "-" * 150 + "+")
+            print(f"Дракон нанес удар рыцарю, но не смог нанести урон. Рыцарь был под щитом")
+            print("+" + "-" * 150 + "+")
+
     
     def dragon_damage(self):
         if self.dragon._hp > 0:
@@ -141,8 +167,11 @@ class Damage():
                 else:
                     self.dragon_damage_whithout_fire_ball()    
             else:
-                print(f"\nДракон проспал свой ход")
-                print(f"У рыцаря все еще {self.knight.health_tracking()} единиц здоровья \n")
+                print(dragon_sleep)
+                print("+" + "-" * 150 + "+")
+                print(f"Дракон проспал свой ход")
+                print(f"У рыцаря все еще {self.knight.health_tracking()} единиц здоровья ")
+                print("+" + "-" * 150 + "+")
  
     def dragon_attack(self):
         if self.knight._hp > 0:
@@ -157,12 +186,19 @@ class Damage():
         if self.knight._hp > 0:
             if random.random() <= 0.75:
                 self.dragon._hp -= self.knight.hit() - self.dragon._defence
-                print(f"\nРыцарь нанес удар и дракон получил {self.knight.hit() - self.dragon._defence} единиц урона")
-                print(f"И теперь у дракона осталось {self.dragon.health_tracking()} единиц здоровья \n")
+                print(knight_att)
+                print("+" + "-" * 150 + "+")
+                print(f"Рыцарь нанес удар и дракон получил {self.knight.hit() - self.dragon._defence} единиц урона")
+                print(f"И теперь у дракона осталось {self.dragon.health_tracking()} единиц здоровья ")
+                print("+" + "-" * 150 + "+")
                 self.dragon.hiroe_lose()
             else:
-                print(f"\nРыцарь не смог попасть и нанести урон дракону")
-                print(f"У дракона все еще {self.dragon.health_tracking()} единиц здоровья \n")
+                print(fack)
+                print("+" + "-" * 150 + "+")
+                print(f"Рыцарь не смог попасть и нанести урон дракону")
+                print(f"У дракона все еще {self.dragon.health_tracking()} единиц здоровья ")
+                print("+" + "-" * 150 + "+")
+
 
     def knight_attack(self):
         if self.dragon._hp > 0: 
@@ -177,15 +213,25 @@ class DragonGame:
     """Здесь происходит сражение"""
  
     damage = Damage()
+    print("\n+" + "-" * 64 + "DRAGONGAME" + "-" * 64 + "+")
+    print(start_game)
+    print(damage.knight)
+    print(damage.dragon)
+    
  
     while damage.LOOP_:
-        action = input("Пожалуйста введите действие рыцаря: attack, pass, defence, potion, infoD, infoK ==> ").lower()
+        print("+" + "-" * 150 + "+")
+        action = input("Пожалуйста введите действие рыцаря: stop, attack, pass, defence, potion, info ==> ").lower()
+        print("+" + "-" * 150 + "+")
         
         if action == "attack":
             damage.knight_attack()
             damage.dragon_attack()
         elif action == "pass":
-            print("\nРыцарь пропускает ход \n")
+            print(pass_)
+            print("+" + "-" * 150 + "+")
+            print("Рыцарь пропускает ход ")
+            print("+" + "-" * 150 + "+")
             damage.dragon_attack()
         elif action == "defence":
             damage.knight.equip_shield()
@@ -193,11 +239,16 @@ class DragonGame:
         elif action == "potion":
             damage.knight.get_potion()
             damage.dragon_attack()
-        elif action == "infoD":
-            print(damage.dragon)
-        elif action == "infoK":
+        elif action == "info":
             print(damage.knight)
+            print(damage.dragon)
+        elif action == "info_k":
+            print(damage.knight)
+        elif action == "stop":
+            break
         else:
-            print("\nТакой команды нет, попробуйте снова \n")  
+            print("+" + "-" * 150 + "+")
+            print("Такой команды нет, попробуйте снова ")
+            print("+" + "-" * 150 + "+")  
  
 d = Damage()
